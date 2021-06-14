fee7d813f9632e3ba7d097ccb1caf34fd0c1e980
Anil Thadani <athadani@stevens.edu>
2021-06-13 18:23:45 -0700

    Update merge-triplets-to-form-target-triplet.py

--- merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -3,10 +3,13 @@ class Solution:
         firstmax, secondmax, thirdmax = target
         current = [0, 0, 0]
         for current_value in triplets:
+            # if any of the triplet's current element is greater than required max value then ignore
             if current_value[0]>firstmax or current_value[1]>secondmax or current_value[2]>thirdmax:
                 continue
+            # if any of the triplet's current element is equal to required max value then consider max of this and last max value
             if current_value[0]==firstmax or current_value[1]==secondmax or current_value[2]==thirdmax:
                 current = [max(current[0],current_value[0]), max(current[1], current_value[1]), max(current[2], current_value[2])]
+            # check if target reached
             if current==target:
                 return True
         return False
