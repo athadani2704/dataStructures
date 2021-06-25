dbc5d547ce70fa489d85c942b76444a9807d052d
Anil Thadani <athadani@stevens.edu>
2021-06-25 09:52:14 -0700

    Time: 606 ms (98.80%), Space: 0B (100.00%) - LeetHub

---
+++ exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+
+
+
+select case when (a.id%2)=0 then a.id-1 
+            when (a.id%2)=1 and (a.id<(select max(id) from seat)) then a.id+1
+            when ((select max(id) from seat)%2)=0 then a.id+1
+            else a.id
+            end as id, a.student from seat a 
+            order by id-- join seat b on a.id=b.id-1
